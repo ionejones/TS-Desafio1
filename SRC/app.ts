5857f1596de60a32f2dc2cfb47a239c85e386e0b
ione <ionejones@gmail.com>
2022-07-07 09:32:33 -0300

    Desafio 1(uso de classe)

---
+++ SRC/app.ts
@@ -0,0 +1,8 @@
+let employee : {code:number,  name:string} =
+{    code : 10,
+    name : 'Ione'
+};
+
+employee.code = 10;
+employee.name = "John";
+console.log(`Meu nome é ${employee.name} e minha matrícula é  ${employee.code}`);
